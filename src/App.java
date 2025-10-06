f4d9c4d5a6d02849bd23975d6ae27094ec968929
Asafa003 <asafadoyin@gmail.com>
2025-10-06 17:22:53 +0100

    first java project

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Printing Hello World to the console on the next line
+        System.out.println("Hello, World!"); 
+        //Printing Welcome message to the console without a new line
+        System.out.print("Welcome to the Java Application!.");
+        System.out.print("Enjoy coding!");
+    }
+}
